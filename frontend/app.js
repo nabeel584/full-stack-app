790db5a13893e5e604f9791873968fe1298debe9
nabeel584 <muhammadnabeel584@gmail.com>
2024-02-29 10:49:00 +0500

    product page

--- frontend/app.js
+++ frontend/app.js
@@ -1,13 +1,18 @@
 
-
-// For SignUP
+//Global
 const username = document.querySelector('#name')
 const email = document.querySelector('#email');
 const password = document.querySelector('#password');
+const loginEmail = document.querySelector('#loginEmail');
+const loginPassword = document.querySelector('#loginPassword');
 const signUpButton = document.querySelector('#signUpButton');
+const signInButton = document.querySelector('#signInButton');
 const displayMessage = document.querySelector('.displayMessage')
+const loginBtnText = document.querySelector('.login-button')
+let API_URI = 'http://localhost:3000/api';
 
-signUpButton.addEventListener('click', (e) => {
+// For SignUP
+signUpButton?.addEventListener('click', (e) => {
   e.preventDefault();
   signUpUser();
 });
@@ -23,7 +28,7 @@ const passwordValue = password.value;
 const usernameValue = username.value
 console.log(passwordValue, emailValue,usernameValue )
 
-  let API_URI = 'http://localhost:3000/api';
+
   const response = await fetch(`${API_URI}/users/signUp`, {
     method: 'POST',
     headers: {
@@ -38,6 +43,45 @@ console.log(passwordValue, emailValue,usernameValue )
 
   const data = await response.json();
   if(data){
+    if(data.message.includes('User account has been created successfully')){
+      loginBtnText.innerHTML = "Go To Login"
+    }
     displayMessage.innerHTML = data.message
   }
 };
+
+//For sign In 
+
+signInButton?.addEventListener('click', (e) => {
+  e.preventDefault();
+  signInUser();
+})
+
+
+const signInUser = async () => {
+
+  const loginEmailValue = loginEmail.value
+  const loginPasswordValue = loginPassword.value
+  console.log(loginEmailValue, loginPasswordValue)
+
+const response = await fetch(`${API_URI}/users/signIn`, {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify({
+    email: loginEmailValue,
+    password: loginPasswordValue
+  })
+})
+
+const data = await response.json()
+displayMessage.innerHTML = data.message
+if(data.message.includes('User Logged In successfully')){
+  window.location.href = 'http://127.0.0.1:5500/frontend/products.html'
+}
+}
+
+// For Home Page
+
+
